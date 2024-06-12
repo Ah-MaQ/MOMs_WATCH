6df90ca79e7d06b11aad410f7bf176bcd41edf52
imssh327 <169126383+imssh327@users.noreply.github.com>
2024-06-12 09:28:23 +0900

    Add files via upload

---
+++ Mom's%20Watch_240611/Mom's%20Watch_240611/login.js
@@ -0,0 +1,3 @@
+document.getElementById('back-button').addEventListener('click', function() {
+    window.location.href = 'popup.html';
+});
---
+++ Mom's%20Watch_240611/Mom's%20Watch_240611/webpack.config.cjs
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: {
+      popup: './popup/popup.js',
+      alarm: './alarm.js',
+      auth: './auth.js', // auth.js 추가
+      background: './background.js',
+      webcam: './cam/webcam.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? false : 'source-map',
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist')
+      },
+      compress: true,
+      port: 9000,
+      open: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
+  };
+};
